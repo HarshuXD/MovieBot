138657bc30e136bf38942938366b3b97e3de8138
aafusam <62737721+aafusam@users.noreply.github.com>
2022-01-25 12:48:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,7 +38,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-FILE_STORE_CHANNEL = int (environ.get ('FILE_STORE_CHANNEL', 0))
+PUBLIC_FILE_STORE = int(engiron.get('PUBLIC_FILE_STORE', 0))
+FILE_STORE_CHANNEL = int (environ.get('FILE_STORE_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Apkapkapak_Movies')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
