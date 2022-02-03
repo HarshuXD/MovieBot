0a9eb903ac698f108c396212fa45914cfdc93eee
aafusam <62737721+aafusam@users.noreply.github.com>
2022-02-03 13:40:13 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -75,7 +75,7 @@ async def answer(bot, query):
             await query.answer(results=[], is_personal=True,
                            cache_time=cache_time,
                            switch_pm_text=str(e)[:63],
-                           switch_pm_parameter="start")
+                           switch_pm_parameter="error")
     else:
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
         if string:
