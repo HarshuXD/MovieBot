a5099d8c1e7bf757d6a71767ef6880ca18a0067b
HarshuXD <95332135+HarshuXD@users.noreply.github.com>
2022-03-14 19:46:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/aafusam/MovieBot.git /MovieBot
+  git clone https://github.com/HarshuXD/MovieBot.git /MovieBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MovieBot
